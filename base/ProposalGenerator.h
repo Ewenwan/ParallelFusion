c90f1850a6ba6d9b267b4fb136d8a7b72993a9e6
Chen Liu <liuchenwustl@gmail.com>
2016-03-06 23:49:47 -0600

    minor changes

--- base/ProposalGenerator.h
+++ base/ProposalGenerator.h
@@ -12,7 +12,7 @@ template<class LabelType> class ProposalGenerator
   virtual LabelSpace<LabelType> getProposal() = 0;
   virtual std::vector<LabelType> getInitialSolution() const = 0;
   virtual void writeSolution(const std::vector<LabelType> &solution, const int thread_index, const int iteration) const {}; //write temporary solution for visualization purpose
-  virtual void setGroundTruthSolution(const std::vector<LabelType> &solution) {}; //set ground truth solution for debugging purpose
+  //virtual void setGroundTruthSolution(const std::vector<LabelType> &solution) {}; //Set ground truth solution for debugging purpose. Better to be set in child classes' constructors.
   
  protected:
   std::vector<LabelType> current_solution_;
