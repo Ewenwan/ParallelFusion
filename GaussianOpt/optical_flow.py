25a72bc57871d275f46f6f114ca72448bc62d2dd
erikwijmans <ewijmans2@gmail.com>
2017-03-06 16:20:49 -0600

    Updated optical flow

--- GaussianOpt/optical_flow.py
+++ GaussianOpt/optical_flow.py
@@ -2,7 +2,6 @@ import subprocess, shlex
 from fastnumbers import fast_real, fast_int
 import numpy as np
 import math
-<<<<<<< HEAD
 from GPyOpt.methods import BayesianOptimization
 import re
 from bopt_utils import Runner
@@ -12,44 +11,69 @@ exe = "/home/vision/erikwijmans/ParallelFusion/build/OpticalFlow/OpticalFlow"
 
 data_base = "/home/vision/erikwijmans/ParallelFusion/OpticalFlow/other-data"
 data_sets = "Beanbags DogDance Grove3 MiniCooper Urban2 Venus Dimetrodon Grove2 Hydrangea RubberWhale Urban3 Walking".strip().split(" ")
+
 searcher = re.compile(".*Final energy:\s*(\d*\.\d*).*")
 runner = Runner(exe, searcher, data_base, "{} -scene_name={}/{} -num_proposals_in_total={} -solution_exchange_interval={} -num_proposals_from_others={} -num_threads=")
-num_threads = 4
 
-def run_command(command):
-  num_tries = 2
-  while num_tries != 0:
-    try:
-      process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-
-      data = process.communicate()
-      print data[0]
-
-      res = np.array([math.log(fast_real(data[0].strip().split("\n")[-1].split(",")[0].split(":")[1]))])
-      print res
-      return res
-    except:
-      num_tries -= 1
-
-  raise RuntimeError
-
-def optical_flow(exchange_amount, num_proposals, exchange_interval):
+def flow(exchange_amount, num_proposals, exchange_interval = 2):
   print num_proposals, exchange_interval, exchange_amount
-  res = np.array([0.0])
+  return runner.run(data_sets, fast_int(num_proposals), fast_int(exchange_interval), fast_int(exchange_amount))
 
-  num_successful = len(data_sets)
-  for data in data_sets:
-    command = "{} -dataset_name={}/{} -num_proposals_in_total={} -solution_exchange_interval={} -num_proposals_from_others={}".format(exe, data_base, data, fast_int(round(num_proposals[0])), fast_int(round(exchange_interval[0])), fast_int(round(exchange_amount[0])))
-    print command
-
-    try:
-      res += run_command(command)
-    except:
-      num_successful -= 1
-
-  res /= num_successful
+def wrapper(evals):
+  print evals
+  res = np.array([flow(*e) for e in evals])
   print res
-  return res
+  return np.atleast_2d(res)
+
+def main():
+  max_num_proposals = 8
+  num_threads = 1
+  while num_threads <= 8:
+    global runner
+    runner = Runner(exe, searcher, data_base, "{} -dataset_name={}/{} -num_proposals_in_total={} -solution_exchange_interval={} -num_proposals_from_others={} -num_threads=" + str(num_threads))
+    space = [
+      {
+        "name" : "exchange_amount",
+        "type" : "discrete",
+        "domain": tuple(range(0, num_threads)),
+        "dimensionality": 1
+      },
+      {
+        "name" : "num_proposals",
+        "type" : "discrete",
+        "domain": tuple(range(1, max_num_proposals + 1)),
+        "dimensionality": 1
+      },
+      {
+        "name" : "exchange_interval",
+        "type" : "discrete",
+        "domain": tuple(range(1, max_num_proposals + 1)),
+        "dimensionality": 1
+      }
+    ]
+
+    constrains = [
+      {
+        "name": "cont_1",
+        "constrain": "x[:, 0] - x[:, 1] + 0.5"
+      }
+    ]
+
+
+    # --- Solve your problem
+    opt = BayesianOptimization(f=wrapper,
+                                  domain=space,
+                                  constrains=constrains)
+
+    opt.run_optimization(max_iter=35,
+                            evaluations_file="opt-flow-evals-{}.txt".format(num_threads),
+                            models_file="opt-flow-model-{}.txt".format(num_threads),
+                            batch_size=5,
+                            evaluator_type="local_penalization")
+
+
+    opt.plot_acquisition("opt-flow")
+    num_threads *= 2
 
 
 if __name__ == '__main__':
