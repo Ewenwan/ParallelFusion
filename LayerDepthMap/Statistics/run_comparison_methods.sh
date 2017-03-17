702456be56556219b01c0c26d6e43eb185c109a6
erikwijmans <ewijmans2@gmail.com>
2017-03-17 10:10:37 -0500

    Updates

--- LayerDepthMap/Statistics/run_comparison_methods.sh
+++ LayerDepthMap/Statistics/run_comparison_methods.sh
@@ -12,27 +12,27 @@ run_test() {
   beta=$3
   gamma=$4
 
-  for i in {1..5}; do
+  for i in {1..3}; do
     ${exe} -scene_name=${data_set} -num_threads=4 -num_proposals_in_total=${alpha} -num_proposals_from_others=${beta} -solution_exchange_interval=${gamma}
     cp ${data_file} "${run_name}_${i}.csv"
   done
 
 }
 
-echo "Running fusion move"
-for i in {1..5}; do
-  ${exe} -scene_name=${data_set} -num_threads=1 -num_proposals_in_total=1 -num_proposals_from_others=0 -solution_exchange_interval=1
-  cp ${data_file} "fusion_move_${i}.csv"
-done
+# echo "Running fusion move"
+# for i in {1..3}; do
+#   ${exe} -scene_name=${data_set} -num_threads=1 -num_proposals_in_total=1 -num_proposals_from_others=0 -solution_exchange_interval=1
+#   cp ${data_file} "fusion_move_${i}.csv"
+# done
 
-echo "Running parallel fusion move"
-run_test "parallel_fusion_move" 1 0 1
+# echo "Running parallel fusion move"
+# run_test "parallel_fusion_move" 1 0 1
 
 echo "Running SF-MF"
 run_test "sf-mf" 1 1 5
 
-echo "Running SF-SS"
-run_test "sf-ss" 3 0 1
+# echo "Running SF-SS"
+# run_test "sf-ss" 3 0 1
 
 echo "Running SF"
 run_test "sf" 3 3 5
